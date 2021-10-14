8fe24a3b62962299bd41f514582cc8c161794e37
bhu <harold9188@gmail.com>
2021-10-14 12:10:44 -0700

    serialize

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const passport = require('passport'); 
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 
@@ -16,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug'); 
 
 app.route('/').get((req, res) => {
   // pass vars to Pug as second arg 
