dfdc0c4437922494abdc6e5782a363f16b218531
bhu <harold9188@gmail.com>
2021-10-21 14:32:37 -0700

    logout and 404

--- server.js
+++ server.js
@@ -71,10 +71,27 @@ myDB(async client => {
       res.redirect('/profile'); 
     }
   )
+
   app
   .route('/profile')
   .get(ensureAuthenticated, (req,res) => {
-     res.render(process.cwd() + '/views/pug/profile');
+     res.render(
+       process.cwd() + '/views/pug/profile',
+       {username: req.user.username}
+      );
+  });
+
+  app.route('/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // missing 404 
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
   });
 
   // Serialization
