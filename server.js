000408d381771e9a297a03f96e3ff134f49ac1e2
bhu <harold9188@gmail.com>
2021-10-21 16:49:06 -0700

    socket io

--- server.js
+++ server.js
@@ -1,54 +1,49 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const ObjectID = require('mongodb').ObjectID;
-
-const routes = require('./routes.js');
-const auth = require('./auth.js'); 
+const session = require('express-session');
+const passport = require('passport');
+const routes = require('./routes');
+const auth = require('./auth.js');
 
 const app = express();
-app.set('view engine', 'pug'); 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug');
+
+fccTesting(app);
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-/* ------- remove basic GET route -------
-app.route('/').get((req, res) => {
-  // pass vars to Pug as second arg 
-  res.render(process.cwd()+'/views/pug', {title: 'Hello', message: 'Please login'});
-}); -------------------------------------
-*/
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-// --------------------- CONNECT DB ---------------------
-myDB(async client => {
+myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
 
   routes(app, myDataBase);
-  auth(app, myDataBase); 
+  auth(app, myDataBase);
 
-}).catch(e => {
+  io.on('connection', (socket) => {
+    console.log('A user has connected');
+  });
+}).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
-// ------------------------------------------------------
 
-// app.listen
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+http.listen(process.env.PORT || 3000, () => {
+  console.log('Deafult 3000. Listening on port ' + process.env.PORT);
 });
